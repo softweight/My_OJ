31c49e8ca7a7edd607fba2b8948211d3205438cc
softweight <diweird610@gmail.com>
2021-12-03 15:40:47 +0800

    DS day9

--- Leetcode/#20_Valid_Parentheses/20.cpp
+++ Leetcode/#20_Valid_Parentheses/20.cpp
@@ -5,30 +5,28 @@
 using namespace std;
 
 bool isValid(string s){
-    if(s.length() %2 != 0) return false;
     vector<char> stack;
-    // sort(s.begin(), s.end());
-    for(auto i :s){    
-        if(i =='(' || i == '[' || i=='{'){
-            stack.push_back(i);
+        for(auto i :s){
+            if(i =='(' || i == '[' || i=='{'){
+                stack.push_back(i);
+            }
+            else{
+                if(stack.size() == 0) return false;
+                if(i==')' && stack[stack.size()-1] != '('){
+                    return false;
+                }
+                else if(i==']' && stack[stack.size()-1] != '['){
+                    return false;
+                }
+                else if(i=='}' && stack[stack.size()-1] != '{'){
+                    return false;
+                }
+                else stack.pop_back();
+            }
         }
-        else{
-            if(stack.size() == 0) return false;
-            if(i==')' && stack[stack.size()-1] != '('){
-                return false;
-            }
-            else if(i==']' && stack[stack.size()-1] != '['){
-                return false;
-            }
-            else if(i=='}' && stack[stack.size()-1] != '{'){
-                return false;
-            }
-            else stack.pop_back();
-        }
-    }
-    if(stack.size() != 0)
-        return false;
-    return true;
+        if(stack.size() != 0) return false;
+
+        return true;
 
 }
 
